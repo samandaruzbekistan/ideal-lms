b88aec7c8663eae39ee08fe484f8db0c8e7fbe62
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-06-10 22:31:19 +0500

    admin login added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\AdminRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    public function login(Request $request){
+    public function __construct(protected AdminRepository $adminRepository)
+    {
+    }
 
-        return response()->json();
+    public function login(Request $request){
+        $admin = $this->adminRepository->getAdmin($request->login);
+        if (!$admin){
+            return response()->json([
+                'success' => false
+            ], 404);
+        }
+        if (Hash::check($request->input('password'), $admin->password)) {
+            $admin->tokens()->delete();
+            $token = $admin->createToken($request->login)->plainTextToken;
+            return response()->json([
+                'success' => true,
+                'data' => $admin,
+                'token' => $token
+            ], 200);
+        }
+        else{
+            return response()->json([
+                'success' => false
+            ], 404);
+        }
+    }
+
+    public function home(){
+        return response()->json([
+            'add' => 44
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::prefix('admin')->group(function () {
     Route::post('login', [AdminController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function (){
+        Route::post('home', [AdminController::class, 'home']);
+    });
 });
